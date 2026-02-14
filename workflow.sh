182bbc52decbabf05e235c4a14b3d05bc5609da9
Vojtěch Brož <vojtechbrozv@gmail.com>
2026-02-14 15:00:11 +0100

    bash script clasifying transversions and translations

---
+++ workflow.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+< "$1" zcat |
+    cut -f 1-8 |
+    grep -v -e "^#" -e "INDEL" |
+    awk '{print($4$5)}'  |
+    sed -E "s/(.)(.),(.)/\1\2\n\1\3/" |  sed -E "s/(.)(.),(.)/\1\2\n\1\3/" |
+    awk '{if ($1 ==  "AG" || $1 == "GA" || $1 == "CT" || $1 =="TC") print($1, "transition"); else print($1, "transversion");}' |
+    tr " " "\t" |
+    sed -E "s/^(.)(.)/\1\t\2/" > results/substitution_type.tsv
